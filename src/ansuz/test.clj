210da969724c0851426970771a339ec840b51e81
Your Name <frbracch@gmail.com>
2013-02-13 22:26:17 +0100

    changed the test function, now generates a big array

--- src/ansuz/test.clj
+++ src/ansuz/test.clj
@@ -10,17 +10,12 @@
 
 (def input "{\"1234567890\": [1,2,3,4,5,6,7,8,9,0], \"true\": true, \"false\": false, \"string\" : \"this will be a very long string\"}")
 
+(defn make-json-array [n in]
+  (let [inputs (map (fn [_] in) (range 0 n))]
+    (str "[" (reduce #(str %2 "," %1) inputs) "]")))
+  
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (time (times 10000 #(parse input))))
-
-
-;; (ns ansuz.test
-;;   (:use ansuz.parsers.json)
-;;   (:genclass))
-
-;; (def input "{\"1234567890\": [1,2,3,4,5,6,7,8,9,0], \"true\": true, \"false\": false \"string\" : \"this will be a very long string\"}")
-
-;; (defn -main [& args]
-;;   (print (times 10000 #(parse input))))
+  (let [input1 (make-json-array 10000 input)]
+    (time (do (parse input1) 'ok))))
