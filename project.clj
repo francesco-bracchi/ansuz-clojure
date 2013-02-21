61d77bede97d4fa8335f734c3133e8273fedacd4
Your Name <frbracch@gmail.com>
2013-02-21 20:36:49 +0100

    actually added license

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject ansuz "0.1.1"
   :description "Ansuz is a language to create parsers for clojure"
   :url "https://github.com/francesco-bracchi/ansuz-clojure"
-  :license 
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-marginalia "0.7.1"]]
   :dependencies [[org.clojure/clojure "1.4.0"]])
 
